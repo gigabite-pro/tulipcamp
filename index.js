d28d67fbd468de64c16fcae4fc99ccb519ece6ff
Vaibhav Sharma <rajmachawal19@gmail.com>
2022-06-28 01:11:44 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const port = 3000 || process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
